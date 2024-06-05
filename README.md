920ad028ba2a0dc00f4678b42de58fe896550bac
Abaad Ali <93701613+abaadali899@users.noreply.github.com>
2024-06-05 14:15:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project:  React Toggle Task
+created with vite
+
+
+# Install with npm:
+
+ npm install
+
+# Run with npm:
+
+ npm run dev
+
+# Open http://localhost:5173/ in your browser 
+ 
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import ToggleItem from './ToggleItem';
+
+const Toggle = ({data}) => {
+  const [active, setActive] = useState(null);
+
+  const handleClick = (id) => {
+    setActive(id == active ? null : id);
+     
+  };
+
+  return (
+    <div>
+      {data.map((item) => (
+        <ToggleItem
+          key={item.id}
+          heading={item.heading}
+          des={item.des}
+          isActive={item.id === active}
+          onClick={() => handleClick(item.id)}  
+          
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Toggle;
+ 
